ea72d0db95508b375bab642fdb6f5f2b6cb2575b
j <j@localhost.localdomain>
2024-08-20 13:53:08 +0200

    Adds regular subscription expenses.

---
+++ jobs/check_subscriptions.php
@@ -0,0 +1,37 @@
+<?php
+require "/home/jovhmax/www/includes/config.php";
+
+$result = $conn->query("SELECT * FROM subscriptions WHERE pay_day = DAY(CURRENT_DATE)");
+
+if ($result->num_rows == 0) {
+    echo "No subscriptions...<br>";
+
+} else {
+    while ($row = $result->fetch_assoc()) {
+
+        $item = $row["name"];
+        $price = $row["price"];
+        $category = $row["category"];
+        $expense_date = $row["pay_day"];
+        $company = $row["company"];
+        $frequency = $row["frequency"];
+
+        if ($frequency == "rok") {
+            $result = $conn->query("SELECT 1 FROM expenses WHERE item = '$item' AND category = '$category' AND price = $price AND YEAR(expense_date) = YEAR(CURRENT_DATE)");
+            if (!$result->num_rows == 0) {
+                continue;
+            }
+        }
+
+        $conn->query("INSERT INTO expenses (item, category, price, expense_date, company) VALUES ('$item', '$category', $price, CURRENT_DATE(), '$company')");
+
+        $conn->query("UPDATE budget SET real_cost = real_cost + $price WHERE category = '$category'");
+
+        echo "Subscription $item: $price zł saved to database<br>";
+    }
+
+    $conn->close();
+    
+}
+
+?>
